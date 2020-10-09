5856a6360c9118fe6e11c6a100866e7414ff624f
mateusoassis <mateusoassis@gmail.com>
2020-10-09 14:05:00 -0300

    3a atividade

---
+++ Assets/Scripts/AtirarObjetos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtirarObjetos : MonoBehaviour
+{
+	public Rigidbody2D projectile;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+			
+			Rigidbody2D clone;
+			Vector3 pos = new Vector3(-8f, -1f,0);
+			
+			clone = Instantiate (projectile, pos, Quaternion.identity) as Rigidbody2D;
+			
+			clone.mass = Random.Range(0.01f, 1.0f);
+			clone.velocity = clone.transform.TransformDirection(Random.Range(3.0f, 10.0f), Random.Range(8f, 14f), 0f);
+			
+				Destroy(clone.gameObject, 5.0f);
+			
+			
+		}
+    }
+}
